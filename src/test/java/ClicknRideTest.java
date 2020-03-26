02a9940ce1b78e85f5e2846a2e152a05ff9e777d
ryanki1 <contactkieran@hotmail.com>
2020-03-26 18:35:46 +0100

    Make use of ExpectedConditions

--- src/test/java/ClicknRideTest.java
+++ src/test/java/ClicknRideTest.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -17,8 +19,17 @@ public class ClicknRideTest {
         ChromeOptions options = new ChromeOptions();
         options.setHeadless(true);
         WebDriver driver = new ChromeDriver(options);
+        WebDriverWait wait = new WebDriverWait(driver, 20);
         driver.get("https://clickandride.dbnetze.com/");
         dbLoginBox = driver.findElement(By.className("db-login-box"));
         assertEquals(dbLoginBox.isDisplayed(), true);
+        WebElement eleUser = driver.findElement(By.cssSelector(".item-user .text-input"));
+        eleUser.sendKeys("cnr");
+        WebElement elePassword = driver.findElement(By.cssSelector(".item-password .text-input"));
+        elePassword.sendKeys("cnr");
+        WebElement eleLogin = driver.findElement(By.cssSelector(".dbLoginButton"));
+        eleLogin.click();
+        String expectedText = "Anmeldedaten sind ungültig oder Passwort ist abgelaufen";
+        wait.until(ExpectedConditions.invisibilityOfElementWithText(By.cssSelector(".validation-error"), expectedText));
     }
 }
