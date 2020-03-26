08efe05ee4834c03c9a3982dadbaab75405cd3d9
ryanki1 <contactkieran@hotmail.com>
2020-03-26 19:37:04 +0100

    Make use of ExpectedConditions

--- src/test/java/ClicknRideTest.java
+++ src/test/java/ClicknRideTest.java
@@ -29,7 +29,9 @@ public class ClicknRideTest {
         elePassword.sendKeys("cnr");
         WebElement eleLogin = driver.findElement(By.cssSelector(".dbLoginButton"));
         eleLogin.click();
-        String expectedText = "Anmeldedaten sind ungültig oder Passwort ist abgelaufen";
-        wait.until(ExpectedConditions.invisibilityOfElementWithText(By.cssSelector(".validation-error"), expectedText));
+        String expectedText = "Anmeldedaten xxxx sind ungültig oder Passwort ist abgelaufen";
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".validation-error")));
+        WebElement eleValidationError = driver.findElement(By.cssSelector(".validation-error"));
+        assertEquals(eleValidationError.getAttribute("innerHTML").trim(), expectedText);
     }
 }
